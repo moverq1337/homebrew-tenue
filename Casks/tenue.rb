0385c8991786e2a9b08e9314fbb247373431ad71
moverq1337 <moverq1337@gmail.com>
2026-09-20 19:40:49 +0300

    tenue 0.2.1

--- Casks/tenue.rb
+++ Casks/tenue.rb
@@ -27,6 +27,7 @@ cask "tenue" do
     выдаёт владелец после оплаты ($5, разово).
 
     CLI ставится отдельно:
+      brew trust moverq1337/tenue
       brew install moverq1337/tenue/tenue-cli
   CAVEATS
 end
